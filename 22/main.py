691edfd13d5a8f3931de172e5b0fb6379f43a47e
fake_state <ntdako@gmail.com>
2023-04-23 00:27:36 +0400

    fix 22 and add 24

--- 22/main.py
+++ 22/main.py
@@ -16,9 +16,11 @@ k = set(a)
 for i in k:
     set_1.add(i) 
 b = [int(x) for x in input().split()] 
-k1 = set(b) for i in k1:
+k1 = set(b) 
+for i in k1:
     set_2.add(i) 
 lok = set_1 & set_2 
 kool = list(lok) 
-kool.sort() for i in kool: 
+kool.sort() 
+for i in kool: 
     print(i, end=' ')
